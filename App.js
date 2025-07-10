fb452081d249896c9b41e7424c22c1a25d7b2f83
Kaushik N <kaushik1603n@gmail.com>
2025-07-10 16:30:22 +0530

    Ep-2

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "head1" }, "Hello 1"),
-    React.createElement("h2", { id: "head2" }, "Hello 2"),
+ React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { id: "head1", key: "h1-child1" }, "Hello 1"),
+    React.createElement("h2", { id: "head2", key: "h2-child1" }, "Hello 2"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "head1" }, "hai 1"),
-    React.createElement("h2", { id: "head2" }, "hai 2"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { id: "head1", key: "h1-child2" }, "hai 1"),
+    React.createElement("h2", { id: "head2", key: "h2-child2" }, "hai 2"),
   ]),
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
 
 root.render(parent);
